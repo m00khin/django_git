9b3a7d20470bc958ae4593339375074ece837954
m00khin <mookhin@mail.ru>
2023-10-12 12:30:32 +0300

    lesson_2 ended

--- lesson1/project_1/app_1/models.py
+++ lesson1/project_1/app_1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField('Название автомобиля', max_length=30)
+
+
--- lesson1/project_1/app_1/views.py
+++ lesson1/project_1/app_1/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from urllib import request
 
-# Create your views here.
+
+def url1(resuest):
+    return HttpResponse('Answer 1')
+
+
+def url2(resuest):
+    return HttpResponse('Answer 2')
+
+
+def url3(resuest):
+    return HttpResponse(request)
