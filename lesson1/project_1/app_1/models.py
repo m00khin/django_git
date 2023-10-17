43e46587290ea5db9cf66fd38c05aaa3a6ff5a48
m00khin <mookhin@mail.ru>
2023-10-17 14:37:19 +0300

    lesson_1.4 ended

--- lesson1/project_1/app_1/models.py
+++ lesson1/project_1/app_1/models.py
@@ -3,8 +3,21 @@ from django.db import models
 class Mebel(models.Model):
     link = models.TextField('Ссылка')
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    description = models.TextField('Описание')
+    description = models.TextField(
+        verbose_name='Описание c Куфара'
+    )
+    parse_datetime = models.DateTimeField(
+        auto_now_add=True,
+        blank=True,
+        verbose_name='Дата прихода к нам'
+    )
+    def get_absolute_url(self):
+        return self.link
 
     def __str__(self):
         return f'{self.price} | {self.description[:60]}'
 
+    class Meta:
+        verbose_name = 'Мебель'
+        verbose_name_plural = 'Мебель'
+        ordering = ['parse_datetime', '-price']
